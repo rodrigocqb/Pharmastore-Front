994d1432294d61a69da720a500ab0fe919d3eb85
rodrigocqb <rodrigocortibarros@gmail.com>
2023-04-05 13:52:56 -0300

    feat: create product api services

---
+++ src/services/productServices.ts
@@ -0,0 +1,17 @@
+import IProduct from "../interfaces/IProduct";
+import { get } from "./api";
+
+export async function getAllProducts(): Promise<IProduct[]> {
+  const response = await get("/products");
+  return response.data as IProduct[];
+}
+
+export async function getProductData(id: string): Promise<IProduct> {
+  const response = await get(`/${id}`);
+  return response.data as IProduct;
+}
+
+export async function searchProducts(query: string): Promise<IProduct[]> {
+  const response = await get(`/search?searchParam=${query}`);
+  return response.data as IProduct[];
+}
