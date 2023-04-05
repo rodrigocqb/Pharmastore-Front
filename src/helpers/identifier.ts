93ed039bb5ccf678593c823ded375fbeec71d6cf
rodrigocqb <rodrigocortibarros@gmail.com>
2023-04-05 19:15:53 -0300

    fix: fix identifier helper function

--- src/helpers/identifier.ts
+++ src/helpers/identifier.ts
@@ -1,5 +1,13 @@
 import { v4 as uuid } from "uuid";
 
 export function getIdentifier(): string {
-  return localStorage.getItem("user") || uuid();
+  const identifier = localStorage.getItem("user");
+
+  if (!identifier) {
+    const newIdentifier = uuid();
+    localStorage.setItem("user", newIdentifier);
+    return newIdentifier;
+  }
+
+  return identifier;
 }
